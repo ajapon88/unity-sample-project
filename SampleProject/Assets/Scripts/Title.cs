22cf67d5dad32c25117bba3c1b2328372bf933e3
ajapon88 <ajapon88@gmail.com>
2018-04-24 02:59:46 +0900

    add display dialog

--- SampleProject/Assets/Scripts/Title.cs
+++ SampleProject/Assets/Scripts/Title.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 public class Title : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+#if UNITY_EDITOR
+		EditorUtility.DisplayDialog("title", "message", "ok");
+#endif
 	}
 	
 	// Update is called once per frame
